0c1d0856c9e6b86b4cf34d62c8bd40bf5ed50880
hzz <3190101611@zju.edu.cn>
2022-08-29 11:10:29 +0800

    Blog build.
    Version 0.1.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,25 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2022/08/28/welcome-to-jekyll.html",
+        "title": "WPS 公式快捷输入的配置方法",
+        "excerpt":"WPS 公式快捷输入的配置方法 1. 设置插入公式快捷键 不同于 WORD 自带了插入公式快捷键，WPS 中需要我们手动添加。以此选择：文件、自定义功能区、键盘快捷方式（自定义），调出自定义键盘窗口。 在 类别 中选择 “插入”选项卡，在 命令 中选择 EquationInsert，在 当前快捷键窗口 可以查看当前已定义的快捷键（默认为空），在 请按新的快捷键 区域录入新的快捷键。如：Alt + =。 2. 掌握 WPS 公式编辑器的快捷键 WPS 公式编辑器无法支持 Latex 语法，鼠标操作速度很慢。因此，掌握常用的快捷键可以大大提高公式插入编辑的速度，甚至在无需鼠标操作的情况下完成几乎所有公式的编辑。 常用 WPS 公式编辑器快捷键 调出选项卡 Alt + F: 文件选项卡 Alt + E: 编辑选项卡 Alt + V: 视图选项卡 Alt + T: 格式选项卡 Alt...","categories": ["others"],
+        "tags": ["WPS","排版"],
+        "url": "/others/2022/08/28/wps-equation-configure.html",
+        "teaser": null
+      },{
+        "title": "Code test",
+        "excerpt":"code test.  ","categories": ["code"],
+        "tags": ["TEST"],
+        "url": "/code/2022/08/29/code-test.html",
+        "teaser": null
+      },{
+        "title": "Life test",
+        "excerpt":"life test.  ","categories": ["life"],
+        "tags": ["TEST"],
+        "url": "/life/2022/08/29/life-test.html",
+        "teaser": null
+      },{
+        "title": "Optics test",
+        "excerpt":"Optics test.  ","categories": ["optics"],
+        "tags": ["TEST"],
+        "url": "/optics/2022/08/29/optics-test.html",
         "teaser": null
       }]
